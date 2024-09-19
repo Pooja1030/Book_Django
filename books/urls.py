2a5cc989ab3630b3a4ddb3f2a9fc46dce05afd8f
Pooja <poojasawant1030@gmail.com>
2024-09-19 11:22:06 +0530

    updated

--- books/urls.py
+++ books/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import AuthorListCreate, BookListCreate, UserRegistrationView,LogoutView, SendMessageView, GetMessagesView, GeminiPredictionView
-from .views import AuthorListCreate, BookListCreate, UserRegistrationView,LogoutView, SendMessageView, GetMessagesView
+
 
 urlpatterns = [
     
